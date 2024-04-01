01547b820097af6b0b0d08eeb0c344bdf8be3d9d
beMimg <bernardommguerreiro@gmail.com>
2024-04-01 16:08:23 +0200

    Add usernameLowerCase to userSchema

--- models/User.js
+++ models/User.js
@@ -14,6 +14,10 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  usernameLowerCase: {
+    type: String,
+    required: true,
+  },
   bio: {
     type: String,
   },
