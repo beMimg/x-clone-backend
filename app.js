9c9b30026ff72949a916b1aabeed6fd941d594e0
beMimg <bernardommguerreiro@gmail.com>
2024-04-02 14:27:00 +0200

    Delete existing cookieparser from app.js

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const mongoose = require("mongoose");
 require("dotenv").config();
-
 // routes
 const apiRouter = require("./routes/api");
 
