94c24e7d8c3bc90705eecf1333c72269e38a56fa
beMimg <bernardommguerreiro@gmail.com>
2024-04-02 13:45:14 +0200

    Modify res.status to res.sendStatus in verifyJWT

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -3,10 +3,10 @@ require("dotenv").config();
 
 const verifyJWT = (req, res, next) => {
   const authHeader = req.headers["authorization"];
-  if (!authHeader) return res.status(401);
+  if (!authHeader) return res.sendStatus(401);
   const token = authHeader.split(" ")[1];
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) return res.status(403);
+    if (err) return res.sendStatus(403);
     req.user = user;
     next();
   });
