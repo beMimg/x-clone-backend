77afb977d9ff4fdc3f6351306ffd76cdea785efc
beMimg <bernardommguerreiro@gmail.com>
2024-04-02 14:27:54 +0200

    Create refresh route for refresh access tokens

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const usersRoute = require("./users");
-const loginRoute = require("./login");
-
+const loginRoute = require("./auth");
+const refreshRoute = require("./refresh");
 router.get("/", function (req, res, next) {
   res.status(200).send({ message: "Api route" });
 });
 
 router.use("/users", usersRoute);
-router.use("/login", loginRoute);
+router.use("/auth", loginRoute);
+router.use("/refresh", refreshRoute);
 
 module.exports = router;
