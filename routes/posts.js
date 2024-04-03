69e4c16c20e57a5f29bc14ea2b2f091fcc5207f2
beMimg <bernardommguerreiro@gmail.com>
2024-04-03 19:52:57 +0200

    Add a route to get all posts

--- routes/posts.js
+++ routes/posts.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const postsController = require("../controllers/postsController");
 
+router.get("/", postsController.getAllPosts);
+
 router.post("/", postsController.createPost);
 
 router.delete("/:post_id", postsController.deletePost);
