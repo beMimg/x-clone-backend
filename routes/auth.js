d3f4cc19789e0c298b41928fa2dd6193eaf95656
beMimg <bernardommguerreiro@gmail.com>
2024-04-07 14:31:50 +0200

    Modify login route to send a token as response

--- routes/auth.js
+++ routes/auth.js
@@ -2,9 +2,37 @@ const express = require("express");
 const passport = require("passport");
 const { authenticate } = require("passport");
 const router = express.Router();
+const generateToken = require("../utils/utils");
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 
-router.post("/", passport.authenticate("local"), (req, res) => {
-  res.status(200).send({ message: "Authenticated", user: req.user.username });
+router.post("/", async (req, res, next) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+
+    if (!user) {
+      return res.status(401).json({ message: "Invalid username" });
+    }
+
+    const passwordMatch = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+
+    if (!passwordMatch) {
+      return res.status(401).json({ message: "Invalid password" });
+    }
+
+    const jwtIssued = generateToken(user);
+
+    return res.json({
+      user: user.username,
+      token: jwtIssued.token,
+      expiesIn: jwtIssued.expiresIn,
+    });
+  } catch (err) {
+    return next(err);
+  }
 });
 
 router.get(
