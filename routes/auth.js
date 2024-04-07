b7e324ec6faa19782823bdb558df4c61054b7731
beMimg <bernardommguerreiro@gmail.com>
2024-04-07 15:18:04 +0200

    Add token as response when authenticating with github strategy

--- routes/auth.js
+++ routes/auth.js
@@ -35,14 +35,20 @@ router.post("/", async (req, res, next) => {
   }
 });
 
+// {session: false} otherwise error will accour saying that session is not required.
+// Not using sessions in this case.
 router.get(
   "/github",
-  passport.authenticate("github", { scope: ["user:email"] })
+  passport.authenticate("github", { session: false, scope: ["user:email"] })
 );
 
+// If a user is sucessfully authenticated, token will be generated and sent back to the user.
 router.get(
   "/github/callback",
-  passport.authenticate("github", { failureRedirect: "/login" }),
+  passport.authenticate("github", {
+    session: false,
+    failureRedirect: "/login",
+  }),
   function (req, res) {
     const user = req.user;
     const jwtIssued = generateToken(user);
