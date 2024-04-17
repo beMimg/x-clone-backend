0a60f2ea1cf8d5c3eb70996a344bb38822759ea4
beMimg <bernardommguerreiro@gmail.com>
2024-04-17 15:12:29 +0200

    fixed domain for github strategy

--- controllers/github2-authController.js
+++ controllers/github2-authController.js
@@ -10,7 +10,8 @@ passport.use(
     {
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/api/auth/github/callback",
+      callbackURL:
+        "https://x-clone-bemimg.netlify.app/api/auth/github/callback",
     },
 
     async function (accessToken, refreshToken, profile, done) {
