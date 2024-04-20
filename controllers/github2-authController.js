beb9bd923b85a6ea521e13f6aa6657ba784e01a6
beMimg <bernardommguerreiro@gmail.com>
2024-04-20 11:36:11 +0200

    GitHub OAuth fix identantion

--- controllers/github2-authController.js
+++ controllers/github2-authController.js
@@ -1,42 +1,42 @@
-// const passport = require("passport");
-// const GitHubStrategy = require("passport-github2").Strategy;
-// const User = require("../models/User");
-// const crypto = require("crypto");
-// const bcrypt = require("bcryptjs");
-// const { randomColor } = require("../utils/utils");
-// const { BASE_URL } = require("../utils/constants");
+const passport = require("passport");
+const GitHubStrategy = require("passport-github2").Strategy;
+const User = require("../models/User");
+const crypto = require("crypto");
+const bcrypt = require("bcryptjs");
+const { randomColor } = require("../utils/utils");
+const { BASE_URL } = require("../utils/constants");
 
-// passport.use(
-//   new GitHubStrategy(
-//     {
-//       clientID: process.env.GITHUB_CLIENT_ID,
-//       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-//       callbackURL: `${BASE_URL}/api/auth/github/callback`,
-//     },
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: `${BASE_URL}/api/auth/github/callback`,
+    },
 
-//     async function (accessToken, refreshToken, profile, done) {
-//       const password = crypto.randomBytes(32).toString("hex");
-//       const hashedPassword = await bcrypt.hash(password, 10);
-//       const user = await User.findOne({ githubId: profile.id });
-//       const randomProfileColor = randomColor();
-//       if (!user) {
-//         const newUser = new User({
-//           first_name: profile.displayName,
-//           githubId: profile.id,
-//           username: profile.username,
-//           usernameLowerCase: profile.username.toLowerCase(),
-//           email: profile.emails[0].value,
-//           profile_pic_src: profile.photos[0].value,
-//           password: hashedPassword,
-//           date: new Date(),
-//           profile_color: randomProfileColor,
-//         });
-//         await newUser.save();
-//         return done(null, newUser);
-//       }
-//       return done(null, user);
-//     }
-//   )
-// );
+    async function (accessToken, refreshToken, profile, done) {
+      const password = crypto.randomBytes(32).toString("hex");
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const user = await User.findOne({ githubId: profile.id });
+      const randomProfileColor = randomColor();
+      if (!user) {
+        const newUser = new User({
+          first_name: profile.displayName,
+          githubId: profile.id,
+          username: profile.username,
+          usernameLowerCase: profile.username.toLowerCase(),
+          email: profile.emails[0].value,
+          profile_pic_src: profile.photos[0].value,
+          password: hashedPassword,
+          date: new Date(),
+          profile_color: randomProfileColor,
+        });
+        await newUser.save();
+        return done(null, newUser);
+      }
+      return done(null, user);
+    }
+  )
+);
 
-// module.exports = passport;
+module.exports = passport;
