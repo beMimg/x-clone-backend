1e44c75d925d10c6193f69f800344abc8429c5d3
beMimg <bernardommguerreiro@gmail.com>
2024-06-28 00:14:00 +0200

    add netlify deploy preview to constants

--- utils/constants.js
+++ utils/constants.js
@@ -7,7 +7,8 @@ let BASE_URL =
 
 let FRONTEND_URL =
   process.env.NODE_ENV === "production"
-    ? "https://x-clone-bemimg.netlify.app"
+    ? "https://x-clone-bemimg.netlify.app" ||
+      "https://deploy-preview-1--x-clone-bemimg.netlify.app/"
     : "http://localhost:5173";
 
 exports.BASE_URL = BASE_URL;
